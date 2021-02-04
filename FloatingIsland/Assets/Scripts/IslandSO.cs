290851dffb4baca9ebf371f3559b49948d8ef891
ignurof <49594591+ignurof@users.noreply.github.com>
2021-02-04 21:33:20 +0100

    restructure project files & setup islandcontroller

---
+++ FloatingIsland/Assets/Scripts/IslandSO.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Islands")]
+public class IslandSO : ScriptableObject
+{
+    public GameObject islandOne;
+    public GameObject islandTwo;
+    public GameObject islandThree;
+    [Header("Selection")]
+    public GameObject activeIsland;
+}
