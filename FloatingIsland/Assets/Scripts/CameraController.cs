22c635897076a6cca3016ec59d84bee93e367e77
ignurof <49594591+ignurof@users.noreply.github.com>
2021-02-04 23:18:56 +0100

    fix last commit

--- FloatingIsland/Assets/Scripts/CameraController.cs
+++ FloatingIsland/Assets/Scripts/CameraController.cs
@@ -10,14 +10,23 @@ public class CameraController : MonoBehaviour
     public Vector3 pos2;
     public Vector3 pos3;
     // Rotation vars
+    public Quaternion rot1;
+    public Quaternion rot2;
+    public Quaternion rot3;
+
+    // Current selection
     private Vector3 activePos;
 
     void Start()
     {
         // Setup cam positions
         pos1 = new Vector3(15, 9, -15);
-        pos2 = new Vector3(-65, 9, 15);
+        pos2 = new Vector3(-65, 9, -15);
         pos3 = new Vector3(15, 9, 65);
+        // Cam rotations
+        rot1 = Quaternion.Euler(25, -45, 0);
+        rot2 = Quaternion.Euler(25, 45, 0);
+        rot3 = Quaternion.Euler(25, -135, 0);
         // Setup start pos
         activePos = pos1;
     }
@@ -25,19 +34,35 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Keyboard.current.aKey.isPressed)
+        if (Keyboard.current.aKey.wasPressedThisFrame)
         {
             if(activePos == pos1)
             {
                 transform.position = pos2;
+                transform.rotation = rot2;
+                activePos = pos2;
+            }
+            else if (activePos == pos3)
+            {
+                transform.position = pos1;
+                transform.rotation = rot1;
+                activePos = pos1;
             }
         }
 
-        if (Keyboard.current.dKey.isPressed)
+        if (Keyboard.current.dKey.wasPressedThisFrame)
         {
             if (activePos == pos1)
             {
                 transform.position = pos3;
+                transform.rotation = rot3;
+                activePos = pos3;
+            }
+            else if (activePos == pos2)
+            {
+                transform.position = pos1;
+                transform.rotation = rot1;
+                activePos = pos1;
             }
         }
     }
