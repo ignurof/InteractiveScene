582ebe04163aef90c7e379c990ec783011338859
ignurof <49594591+ignurof@users.noreply.github.com>
2021-02-05 04:59:01 +0100

    smooth camera

--- FloatingIsland/Assets/Scripts/CameraController.cs
+++ FloatingIsland/Assets/Scripts/CameraController.cs
@@ -6,16 +6,22 @@ using UnityEngine.InputSystem;
 public class CameraController : MonoBehaviour
 {
     // Position vars
-    public Vector3 pos1;
-    public Vector3 pos2;
-    public Vector3 pos3;
+    private Vector3 pos1;
+    private Vector3 pos2;
+    private Vector3 pos3;
+    private Vector3 posHolder;
     // Rotation vars
-    public Quaternion rot1;
-    public Quaternion rot2;
-    public Quaternion rot3;
+    private Quaternion rot1;
+    private Quaternion rot2;
+    private Quaternion rot3;
+
+    // Smooth movement
+    private float speed = 40f;
+    private bool canMove;
+    private bool isMoving;
 
     // Current selection
-    private Vector3 activePos;
+    //private Vector3 activePos;
     public int island; // 1, 2, 3
 
     void Start()
@@ -29,45 +35,69 @@ public class CameraController : MonoBehaviour
         rot2 = Quaternion.Euler(25, 45, 0);
         rot3 = Quaternion.Euler(25, -135, 0);
         // Setup start pos
-        activePos = pos1;
+        //activePos = pos1;
         island = 1;
+        transform.position = pos1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Keyboard.current.aKey.wasPressedThisFrame)
+        float step = speed * Time.deltaTime;
+
+        // Move smoothly towards position
+        if (canMove)
         {
-            if(activePos == pos1)
+            isMoving = true;
+            transform.position = Vector3.MoveTowards(transform.position, posHolder, step);
+            if (transform.position == posHolder)
             {
-                transform.position = pos2;
-                transform.rotation = rot2;
-                activePos = pos2;
+                canMove = false;
+                isMoving = false;
+
+                if (posHolder == pos1)
+                    transform.rotation = rot1;
+                else if (posHolder == pos2)
+                    transform.rotation = rot2;
+                else if (posHolder == pos3)
+                    transform.rotation = rot3;
+            }
+        }
+
+        if (Keyboard.current.aKey.wasPressedThisFrame && !isMoving)
+        {
+            canMove = true;
+            if(transform.position == pos1)
+            {
+                //transform.position = pos2;
+                //transform.rotation = rot2;
+                posHolder = pos2;
                 island = 2;
             }
-            else if (activePos == pos3)
+            else if (transform.position == pos3)
             {
-                transform.position = pos1;
-                transform.rotation = rot1;
-                activePos = pos1;
+                //transform.position = pos1;
+                //transform.rotation = rot1;
+                posHolder = pos1;
                 island = 1;
             }
         }
 
-        if (Keyboard.current.dKey.wasPressedThisFrame)
+        if (Keyboard.current.dKey.wasPressedThisFrame && !isMoving)
         {
-            if (activePos == pos1)
+            canMove = true;
+            if (transform.position == pos1)
             {
-                transform.position = pos3;
-                transform.rotation = rot3;
-                activePos = pos3;
+                //transform.position = pos3;
+                //transform.rotation = rot3;
+                posHolder = pos3;
                 island = 3;
             }
-            else if (activePos == pos2)
+            else if (transform.position == pos2)
             {
-                transform.position = pos1;
-                transform.rotation = rot1;
-                activePos = pos1;
+                //transform.position = pos1;
+                //transform.rotation = rot1;
+                posHolder = pos1;
                 island = 1;
             }
         }
---
+++ FloatingIsland/Assets/Scripts/ExampleSmooth.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleSmooth : MonoBehaviour
+{
+    public float speed;
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y + 10f, transform.position.z), step);
+    }
+}
